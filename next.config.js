dd00383214f9767881fe0dd3fb62e46fe7b73089
Collen Godfrey <cpgodf@gmail.com>
2025-10-24 17:10:31 -0400

    fix: remove duplicate redirect causing infinite loop

--- next.config.js
+++ next.config.js
@@ -36,21 +36,6 @@ const nextConfig = {
         ]
       }
     ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.fastheictojpg.com'
-          }
-        ],
-        destination: 'https://fastheictojpg.com/:path*',
-        permanent: true
-      }
-    ];
   }
 };
 
